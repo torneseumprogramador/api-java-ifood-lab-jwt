434a0a41f867aabd82172eb0df3c3d16cf326ac2
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-03-24 14:32:05 -0300

    criptografia de senha

--- src/main/java/br/com/kintsugi/apirest/apirest/controllers/LoginController.java
+++ src/main/java/br/com/kintsugi/apirest/apirest/controllers/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.kintsugi.apirest.apirest.domain.entidade.Administrador;
 import br.com.kintsugi.apirest.apirest.domain.repo.AdministradorRepo;
+import br.com.kintsugi.apirest.apirest.infraestrutura.seguranca.cripto.Criptografia;
 import br.com.kintsugi.apirest.apirest.infraestrutura.seguranca.jwt.GeradorDeToken;
 import br.com.kintsugi.apirest.apirest.model_views.AdministradorToken;
 import br.com.kintsugi.apirest.apirest.model_views.LoginModel;
@@ -20,17 +21,20 @@ public class LoginController {
   private AdministradorRepo repo;
 
   @PostMapping("/login")
-  public ResponseEntity<AdministradorToken> login(@RequestBody LoginModel login){
+  public ResponseEntity<AdministradorToken> login(@RequestBody LoginModel login) throws Exception{
     Administrador adm = (Administrador)repo.findByEmail(login.getEmail());
     if(adm == null){
       AdministradorToken admToken = new AdministradorToken();
       admToken.setErro("Administrador não cadastrado");
       return ResponseEntity.status(401).body(admToken);
     }
-    else if(!adm.getSenha().equals(login.getSenha())){
-      AdministradorToken admToken = new AdministradorToken();
-      admToken.setErro("Email ou senha inválidos");
-      return ResponseEntity.status(401).body(admToken);
+    else{
+      String senhaCripto = Criptografia.gerar(login.getSenha());
+      if(!adm.getSenha().equals(senhaCripto)){
+        AdministradorToken admToken = new AdministradorToken();
+        admToken.setErro("Email ou senha inválidos");
+        return ResponseEntity.status(401).body(admToken);
+      }
     }
 
     String token = GeradorDeToken.criarToken(adm);
--- src/main/java/br/com/kintsugi/apirest/apirest/infraestrutura/seguranca/jwt/GeradorDeToken.java
+++ src/main/java/br/com/kintsugi/apirest/apirest/infraestrutura/seguranca/jwt/GeradorDeToken.java
@@ -21,10 +21,12 @@ public class GeradorDeToken {
 	// puro preciosismo para clean code
 	private static final int    SEGUNDOS = 1000;
 	private static final int    MINUTOS  = 60*SEGUNDOS;
+	private static final int    HORAS    = 60*MINUTOS;
+	private static final int    DIAS     = 24*HORAS;
 	
 	private static final String HEADER = "Authorization";  // cabecalho http
 	private static final String PREFIX = "Bearer ";        // prefixo do token
-	private static final long   EXPIRATION = 5*MINUTOS;    // tempo de validade
+	private static final long   EXPIRATION = 1*DIAS;    // tempo de validade
 	private static final String SECRET_KEY = "3c0MMerc3Do1f00dP@r@T3st3sD3JWT*";  // palavra chave do token
 	private static final String EMISSOR    = "ProfIsidroIfood";
 	
